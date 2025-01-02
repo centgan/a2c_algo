4edd97497d454012975f37e50b461394101848b3
centgan <ericonsmoke@gmail.com>
2025-01-02 17:37:09 -0500

    write year data to file and read from file

--- indicators.py
+++ indicators.py
@@ -3,11 +3,21 @@
 # Data should be the previous 60 days worth of data with the most recent being at the end of the list
 
 import pandas as pd
-from datetime import datetime
 import numpy as np
-import time
-from tensorflow.keras import datasets, layers, models
 
+
+def calculate_ema(prices, period):
+    prices = np.array(prices)
+
+    alpha = 2 / (period + 1)
+
+    ema = np.zeros_like(prices)
+    ema[period-1] = np.mean(prices[:period])
+
+    for i in range(period, len(prices)):
+        ema[i] = alpha * prices[i] + (1 - alpha) * ema[i - 1]
+
+    return ema
 
 # indicators should return with a constant length array
 # For the research paper method the first 60 entries consist of rsi and the next 60 consist of macd (for now forget
@@ -306,80 +316,177 @@ class Indicators:
             self.news_last_values.append(current_news.max())
 
 
-if __name__ == '__main__':
-    test_data = [[2228.2, 2228.2, 2228.2, 2228.2, '2011-01-13 08:00:00'],
-                 [2228.2, 2228.2, 2228.2, 2228.2, '2011-01-13 08:01:00'],
-                 [2228.2, 2228.2, 2228.2, 2228.2, '2011-01-13 08:02:00'],
-                 [2228.2, 2228.2, 2228.2, 2228.2, '2011-01-13 08:03:00'],
-                 [2228.4, 2228.4, 2228.4, 2228.4, '2011-01-13 08:04:00'],
-                 [2228.2, 2228.0, 2228.2, 2228.2, '2011-01-13 08:05:00'],
-                 [2227.9, 2227.9, 2227.9, 2227.9, '2011-01-13 08:06:00'],
-                 [2227.9, 2227.9, 2227.9, 2227.9, '2011-01-13 08:07:00'],
-                 [2227.7, 2227.5, 2227.7, 2227.5, '2011-01-13 08:08:00'],
-                 [2228.2, 2228.2, 2228.2, 2228.2, '2011-01-13 08:09:00'],
-                 [2227.9, 2227.9, 2227.9, 2227.9, '2011-01-13 08:10:00'],
-                 [2227.9, 2227.9, 2227.9, 2227.9, '2011-01-13 08:11:00'],
-                 [2227.7, 2227.7, 2227.7, 2227.7, '2011-01-13 08:12:00'],
-                 [2227.7, 2227.7, 2227.7, 2227.7, '2011-01-13 08:13:00'],
-                 [2227.9, 2227.9, 2227.9, 2227.9, '2011-01-13 08:14:00'],
-                 [2227.9, 2227.9, 2227.9, 2227.9, '2011-01-13 08:15:00'],
-                 [2227.9, 2227.9, 2227.9, 2227.9, '2011-01-13 08:16:00'],
-                 [2227.9, 2227.9, 2227.9, 2227.9, '2011-01-13 08:17:00'],
-                 [2227.9, 2227.9, 2227.9, 2227.9, '2011-01-13 08:18:00'],
-                 [2227.9, 2227.9, 2227.9, 2227.9, '2011-01-13 08:19:00'],
-                 [2227.9, 2227.9, 2227.9, 2227.9, '2011-01-13 08:20:00'],
-                 [2227.9, 2227.9, 2227.9, 2227.9, '2011-01-13 08:21:00'],
-                 [2228.2, 2228.2, 2228.2, 2228.2, '2011-01-13 08:22:00'],
-                 [2228.2, 2228.2, 2228.2, 2228.2, '2011-01-13 08:23:00'],
-                 [2228.2, 2228.2, 2228.2, 2228.2, '2011-01-13 08:24:00'],
-                 [2227.9, 2227.9, 2227.9, 2227.9, '2011-01-13 08:25:00'],
-                 [2227.9, 2227.9, 2227.9, 2227.9, '2011-01-13 08:26:00'],
-                 [2227.9, 2227.9, 2227.9, 2227.9, '2011-01-13 08:27:00'],
-                 [2227.9, 2227.9, 2227.9, 2227.9, '2011-01-13 08:28:00'],
-                 [2227.9, 2227.9, 2227.9, 2227.9, '2011-01-13 08:29:00'],
-                 [2228.2, 2228.2, 2228.2, 2228.2, '2011-01-13 08:30:00'],
-                 [2228.2, 2228.2, 2228.2, 2228.2, '2011-01-13 08:31:00'],
-                 [2228.2, 2228.2, 2228.2, 2228.2, '2011-01-13 08:32:00'],
-                 [2227.9, 2227.6, 2227.9, 2227.6, '2011-01-13 08:33:00'],
-                 [2227.9, 2227.6, 2227.9, 2227.6, '2011-01-13 08:34:00'],
-                 [2227.9, 2227.6, 2227.9, 2227.6, '2011-01-13 08:35:00'],
-                 [2227.9, 2227.6, 2227.9, 2227.6, '2011-01-13 08:36:00'],
-                 [2227.9, 2227.6, 2227.9, 2227.6, '2011-01-13 08:37:00'],
-                 [2228.4, 2228.4, 2228.6, 2228.6, '2011-01-13 08:38:00'],
-                 [2228.4, 2228.4, 2228.4, 2228.4, '2011-01-13 08:39:00'],
-                 [2228.4, 2228.4, 2228.4, 2228.4, '2011-01-13 08:40:00'],
-                 [2228.2, 2228.0, 2228.2, 2228.0, '2011-01-13 08:41:00'],
-                 [2228.2, 2228.0, 2228.2, 2228.0, '2011-01-13 08:42:00'],
-                 [2228.7, 2228.7, 2230.0, 2229.7, '2011-01-13 08:43:00'],
-                 [2227.4, 2227.4, 2227.4, 2227.4, '2011-01-13 08:44:00'],
-                 [2227.7, 2227.5, 2227.7, 2227.5, '2011-01-13 08:45:00'],
-                 [2227.7, 2227.5, 2227.7, 2227.5, '2011-01-13 08:46:00'],
-                 [2227.7, 2227.5, 2227.7, 2227.5, '2011-01-13 08:47:00'],
-                 [2227.7, 2227.5, 2227.7, 2227.5, '2011-01-13 08:48:00'],
-                 [2227.7, 2227.5, 2227.7, 2227.5, '2011-01-13 08:49:00'],
-                 [2227.7, 2227.5, 2227.7, 2227.5, '2011-01-13 08:50:00'],
-                 [2227.7, 2227.7, 2227.7, 2227.7, '2011-01-13 08:51:00'],
-                 [2227.7, 2227.7, 2227.7, 2227.7, '2011-01-13 08:52:00'],
-                 [2227.9, 2227.9, 2227.9, 2227.9, '2011-01-13 08:53:00'],
-                 [2227.9, 2227.9, 2227.9, 2227.9, '2011-01-13 08:54:00'],
-                 [2227.7, 2227.7, 2227.7, 2227.7, '2011-01-13 08:55:00'],
-                 [2227.7, 2227.7, 2227.7, 2227.7, '2011-01-13 08:56:00'],
-                 [2227.9, 2227.9, 2227.9, 2227.9, '2011-01-13 08:57:00'],
-                 [2227.9, 2227.9, 2227.9, 2227.9, '2011-01-13 08:58:00'],
-                 [2227.7, 2227.7, 2227.7, 2227.7, '2011-01-13 08:59:00']]
+class BatchIndicators:
+    def __init__(self, year_data_filename, year_data_shape, rsi_flag=False, mac_flag=False, ob_flag=False, fvg_flag=False, news_flag=False):
+        self.list_data = np.memmap(year_data_filename, dtype=object, mode='r', shape=year_data_shape)
+        self.rsi_flag = rsi_flag
+        self.mac_flag = mac_flag
+        self.ob_flag = ob_flag
+        self.fvg_flag = fvg_flag
+        self.news_flag = news_flag
 
-    next = [2227.9, 2227.9, 2227.9, 2227.9, '2011-01-13 01:00:00']
-    ends = []
-    a = Indicators(test_data, rsi_flag=True)
-    print(a.rsi_last_values)
-    for i in range(0, 10):
-        start = time.time()
-        a.rsi_init()
-        b = time.time() - start
-        print(b)
-        ends.append(b)
-    print(sum(ends)/10)
-    print(a.rsi_last_values)
+        self.year_indicator_filename = 'year_indicator.dat'
+        self.year_indicator_shape = ()
+
+        self.rsi_need = []
+        self.mac_need = []
+        self.df = pd.read_csv('eco_news.csv')
+        self.df['Time'] = pd.to_datetime(self.df['Time'], format='%I:%M%p')
+        self.df['Time'] = self.df['Time'].dt.strftime('%H:%M:%S')
+
+        self.rsi_last_values = []
+        self.mac_last_values = [[], []]  # list of 2 items, the first is list of macd and the second is list of trigger
+        self.ob_up_last_values = []
+        self.ob_down_last_values = []
+        self.fvgs_up_last_values = []
+        self.fvgs_down_last_values = []
+        self.news_last_values = []
+
+        self.batch_process()
+
+    def batch_process(self):
+        outputting = []
+        if self.rsi_flag:
+            outputting.append(self.rsi_calculate())
+        if self.mac_flag:
+            outputting.append(self.mac_calculate())
+        # if self.ob_flag:
+        #     self.ob_calculate()
+        # if self.fvg_flag:
+        #     self.fvg_calculate()
+        # if self.news_flag:
+        #     self.news_calculate()
+        outputting = np.array(outputting)
+        self.year_indicator_shape = outputting.shape
+        arr = np.memmap(self.year_indicator_filename, dtype='float32', mode='w+', shape=self.year_indicator_shape)
+        for i in range(self.year_indicator_shape[0]):
+            arr[i] = outputting[i]
+        arr.flush()
+        return outputting
+
+    def rsi_calculate(self):
+        period = 14
+        closes = np.array([data[3] for data in self.list_data])
+
+        price_changes = np.diff(closes)
+        gains = np.where(price_changes > 0, price_changes, 0)
+        losses = np.where(price_changes < 0, -price_changes, 0)
+
+        avg_gain = np.zeros_like(closes)
+        avg_loss = np.zeros_like(closes)
+
+        avg_gain[period] = np.mean(gains[:period])
+        avg_loss[period] = np.mean(losses[:period])
+
+        for i in range(period+1, len(closes)):
+            avg_gain[i] = (avg_gain[i - 1] * (period - 1) + gains[i-1]) / period
+            avg_loss[i] = (avg_loss[i - 1] * (period - 1) + losses[i-1]) / period
+
+        rs = avg_gain / avg_loss
+        rsi = 100 - (100 / (1 + rs))
+        rsi[:period] = 0
+
+        return rsi
+
+    def mac_calculate(self):
+        short_period = 12
+        long_period = 26
+        signal_period = 9
+        closes = [data[3] for data in self.list_data]
+
+        ema_short = calculate_ema(closes, short_period)
+        ema_long = calculate_ema(closes, long_period)
+        # print(ema_short, ema_long)
+
+        # Calculate the MACD Line (difference between fast and slow EMAs)
+        macd_line = np.zeros_like(closes)
+        macd_line[25:] = np.array(ema_short)[25:] - np.array(ema_long)[25:]
+
+        # Calculate the Signal Line (9-period EMA of the MACD Line)
+        signal_line = np.zeros_like(closes)
+        signal_line[25:] = calculate_ema(macd_line[25:], signal_period)
+        # signal_line[34:] = calculate_ema(macd_line[25:], signal_period)
+
+        # Return the MACD Line and Signal Line (same length as the input data)
+        return macd_line
+
+
+if __name__ == '__main__':
+    test_data = [[19629,19645.25,19614.5,19623.5],
+[19623.25,19649.75,19623,19641.5],
+[19641,19672.75,19640.75,19667.5],
+[19665.5,19673.75,19660,19661.5],
+[19662.25,19665.25,19653.25,19656],
+[19657,19659.25,19650.25,19654],
+[19653.25,19664,19652.5,19662.25],
+[19662.25,19662.75,19655.5,19657],
+[19657.5,19658.75,19656,19658],
+[19657,19658.75,19655,19655],
+[19655,19663,19653.25,19661.75],
+[19662,19663.25,19657.25,19661.25],
+[19661,19663.5,19659,19660],
+[19660.25,19662.75,19642.25,19645.75],
+[19646.75,19654,19646.25,19653],
+[19653.5,19654,19645.75,19646.5],
+[19646.75,19647,19640.25,19645],
+[19645.5,19647.25,19640,19640],
+[19640.25,19640.5,19633,19635.5],
+[19638,19648.5,19637.75,19641.5],
+[19641.5,19642.75,19640,19641.25],
+[19641.25,19641.25,19639.25,19639.75],
+[19639.75,19642.5,19635.5,19639.75],
+[19639.75,19639.75,19636.75,19637.5],
+[19638.75,19641,19627.5,19631.25],
+[19632.5,19633,19621,19629],
+[19629.25,19636.25,19627.25,19633.75],
+[19635,19647,19634.25,19637.25],
+[19636.75,19642,19633,19641],
+[19641,19643,19637,19638],
+[19638,19643.25,19638,19641.25],
+[19642.75,19647,19637.5,19637.5],
+[19639,19643.25,19639,19642],
+[19641.5,19643,19638.25,19643],
+[19643.75,19644,19640.75,19643],
+[19643.25,19644,19640,19640.5],
+[19641.5,19644,19640,19644],
+[19644.5,19644.5,19641.75,19643.5],
+[19643.25,19644.25,19641,19644.25],
+[19642.25,19645,19642.25,19645],
+[19643.25,19643.25,19638,19642.5],
+[19642.25,19642.25,19636.25,19640],
+[19640,19640.5,19634.5,19636.5],
+[19637,19638.25,19635.5,19636.5],
+[19635.25,19636,19632,19633.75],
+[19634.75,19638,19631,19637],
+[19638,19646,19638,19644],
+[19644.25,19645,19640,19641],
+[19641.5,19643,19641.5,19641.5],
+[19641.5,19643.5,19641.5,19643.5],
+[19642.75,19644,19640,19644],
+[19642.25,19648,19642.25,19647],
+[19647.5,19649.25,19643.5,19646.5],
+[19648.25,19651.25,19647.5,19649.5],
+[19647.25,19653.75,19647.25,19653.25],
+[19652,19655,19648.75,19650],
+[19649.25,19652.25,19647.75,19650.5],
+[19650.25,19651.75,19646.75,19650],
+[19651.25,19651.5,19648.25,19648.25],
+[19649,19651.5,19644.25,19646.75]]
+
+    a = BatchIndicators(test_data, mac_flag=True)
+    print(a.state_space)
+    # next = [2227.9, 2227.9, 2227.9, 2227.9, '2011-01-13 01:00:00']
+    # ends = []
+    # a = Indicators(test_data, rsi_flag=True)
+    # print(a.rsi_last_values)
+    # for i in range(0, 10):
+    #     start = time.time()
+    #     a.rsi_init()
+    #     b = time.time() - start
+    #     print(b)
+    #     ends.append(b)
+    # print(sum(ends)/10)
+    # print(a.rsi_last_values)
     # print(a.news_last_values)
     # print(a.get_indicators(next))
     # a.economic_news()
